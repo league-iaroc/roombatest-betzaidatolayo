3b0b607e90d8de4c8129826929628924342120bd
League <Level 2>
2017-06-10 11:28:27 -0700

    i made a robot walk

--- src/level1/Challenge.java
+++ src/level1/Challenge.java
@@ -19,8 +19,16 @@ public class Challenge extends Head {
 	public void initialize() {
 		//write code here!
 		
-		
-		
+		driveDirect(500,500);
+		sleep(3000);
+		driveDirect(100,500);
+		sleep(2300/2);
+		driveDirect(500,500);
+		sleep(7000);
+	driveDirect(200,300);
+	
+	
+	
 	}
 
 	public void loop() {
